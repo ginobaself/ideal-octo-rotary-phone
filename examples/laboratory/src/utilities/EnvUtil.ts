c94ce9dfab64efe3c70634267450bbb1afbc861c
lizzy <ginobaself@gmail.com>
2023-02-06 19:38:05 +0100

    Decouple

--- examples/laboratory/src/utilities/EnvUtil.ts
+++
@@ -1,8 +0,0 @@
-export function getProjectId() {
-  const projectId = process.env.NEXT_PUBLIC_PROJECT_ID
-  if (!projectId) {
-    throw new Error('NEXT_PUBLIC_PROJECT_ID is not defined')
-  }
-
-  return projectId
-}
